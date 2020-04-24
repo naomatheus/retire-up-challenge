bf1b3eadf960e11e9fc719f75d8cb033220885fd
matthew Robinson <matthewrobinson@matthews-Air-3.attlocal.net>
2020-04-23 21:19:52 -0500

    import data locally

--- src/Column.js
+++ src/Column.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import stockdata from './data/stockdata.json' 
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table-next'
+import Table from './Table.js'
 
 class Column extends Component {
     constructor(props){
         super(props)
 
         this.state = {
-            isFetching: false,
-            data: this.data
+            data: stockdata
         }
+    }
+
+    mapStockData = () => {
+        // set isMapping to true
+        this.setState({
+            ...this.state,
+            isMapping: true,
+        })
+        // use map() to loop over all items in JSON obj
+
+        const mappedData = this.state.data.map((data) => {
+            console.log(data)
+
+            
+        })
+        
+        
+    }
+
+    componentDidMount(){
+        this.mapStockData();
     }
 
     render() {
         return (
-
-            <div>Table will go here inside of Column component</div>
+            <Fragment>
+                <div>Table will go here inside of Column component</div>
+                <Table props={this.state.data}/>
+            </Fragment>
         )
     }
 
--- src/Table.js
+++ src/Table.js
@@ -1,14 +1,36 @@
 import React, {Fragment, Component} from 'react'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table-next'
 
+// class Table extends Component {
+//     constructor(props){
+//         super(props)
+
+//         this.state = {
+//             empty: true
+//         }
+//     }
+
+//     render() {
+//         return (
+            
+//             <BootstrapTable>
+
+//             </BootstrapTable>
+
+//         )
+//     }
+// }
 
 const Table = (props) => {
-    return(
-
+    return (
         <div>
             Pass data as props to me
             <BootstrapTable>
-
+                data={props.data}
+                
+                <TableHeaderColumn isKey datafield='year' />
+                Text inside of table
+                
             </BootstrapTable>
         </div>
 
